5a48e9abcd8133769f22951a900f98d30d1a45a3
Vvvvvaleriia <reznikvaleria31@gmail.com>
2026-03-23 20:42:42 +0200

    Add all cache eviction policies

--- lab3/memo.js
+++ lab3/memo.js
@@ -1,23 +1,73 @@
 "use strict";
 
-function memoize(fn, limit) {
+function memoize(fn, options = {}) {
+	const {
+		limit = Infinity,
+		strategy = "LRU",
+		timeToLive = 0,
+		customFn = null,
+	} = options;
+
 	const cache = new Map();
-	return (...args) => {
-		const key = JSON.stringify(args);
 
-		if (cache.has(key)) {
-			const newValue = cache.get(key);
-			cache.delete(key);
-			cache.set(key, newValue);
-			return newValue;
-		}
+	function cutCache() {
+		if (cache.size < limit) return;
 
-		if (cache.size >= limit) {
+		if (strategy === "LRU") {
 			const oldKey = cache.keys().next().value;
 			cache.delete(oldKey);
+		} else if (strategy === "LFU") {
+			let minCount = Infinity;
+			let keyToDelete = null;
+
+			for (const [key, value] of cache) {
+				if (value.count < minCount) {
+					minCount = value.count;
+					keyToDelete = key;
+				}
+			}
+			if (keyToDelete !== null) cache.delete(keyToDelete);
+		} else if (strategy === "TTL") {
+			const now = Date.now();
+			for (const [key, value] of cache) {
+				if (now - value.timestamp >= timeToLive) cache.delete(key);
+			}
+		} else if (strategy === "CUSTOM" && typeof customFn === "function") {
+			const keyToDelete = customFn(cache);
+			if (keyToDelete) cache.delete(keyToDelete);
 		}
+	}
+
+	return (...args) => {
+		const key = JSON.stringify(args);
+		const now = Date.now();
+
+		if (cache.has(key)) {
+			const value = cache.get(key);
+			value.count++;
+			value.timestamp = now;
+
+			if (strategy === "LRU") {
+				cache.delete(key);
+				cache.set(key, value);
+			}
+			return value.value;
+		}
+
 		const result = fn(...args);
-		cache.set(key, result);
+		cache.set(key, { value: result, count: 1, timestamp: now });
+
+		cutCache();
+
 		return result;
 	};
 }
+
+function sum(a, b) {
+	console.log("Add sum");
+	return a + b;
+}
+
+const memoAdd = memoize(sum, { limit: 3, strategy: "LRU" });
+console.log(memoAdd(2, 3));
+console.log(memoAdd(2, 3));
