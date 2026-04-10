d2ea5f8706ce692aa5bb7c36719533232992f94f
Vvvvvaleriia <reznikvaleria31@gmail.com>
2026-04-10 15:19:24 +0300

    Add dequeue method

--- lab4/queue.js
+++ lab4/queue.js
@@ -1,6 +1,6 @@
 "use strict";
 
-class biDirectionalPriorityQueue {
+class BiDirectionalPriorityQueue {
 	constructor() {
 		this.queue = [];
 		this.queueCounter = 0;
@@ -59,16 +59,84 @@ class biDirectionalPriorityQueue {
 			return minPriority.item;
 		}
 	}
+
+	dequeue(option) {
+		if (this.queue.length === 0) return null;
+
+		if (option === "oldest") {
+			let oldest = this.queue[0];
+			for (const item of this.queue) {
+				if (item.priorityIndex < oldest.priorityIndex) {
+					oldest = item;
+				}
+			}
+			let index = this.queue.indexOf(oldest);
+			this.queue.splice(index, 1);
+
+			return oldest.item;
+		}
+
+		if (option === "newest") {
+			let newest = this.queue[0];
+			for (const item of this.queue) {
+				if (item.priorityIndex > newest.priorityIndex) {
+					newest = item;
+				}
+			}
+			let index = this.queue.indexOf(newest);
+			this.queue.splice(index, 1);
+
+			return newest.item;
+		}
+
+		if (option === "highest") {
+			let maxPriority = this.queue[0];
+			for (const item of this.queue) {
+				if (item.priority > maxPriority.priority) {
+					maxPriority = item;
+				}
+			}
+
+			let index = this.queue.indexOf(maxPriority);
+			this.queue.splice(index, 1);
+
+			return maxPriority.item;
+		}
+
+		if (option === "lowest") {
+			let minPriority = this.queue[0];
+			for (const item of this.queue) {
+				if (item.priority < minPriority.priority) {
+					minPriority = item;
+				}
+			}
+
+			let index = this.queue.indexOf(minPriority);
+			this.queue.splice(index, 1);
+
+			return minPriority.item;
+		}
+	}
 }
 
 const test = new biDirectionalPriorityQueue();
-test.enqueue("dog", 1);
-test.enqueue("cat", 4);
+test.enqueue("dog", 4);
+test.enqueue("horse", 3);
+test.enqueue("cat", 1);
 test.enqueue("fish", 2);
+test.enqueue("cow", 6);
+test.enqueue("hamster", 5);
 
-console.log(`The oldest is ${test.peek("oldest")}`);
-console.log(`The newest is ${test.peek("newest")}`);
-console.log(`The highest priority has ${test.peek("highest")}`);
-console.log(`The lowest priority has ${test.peek("lowest")}`);
+console.log(`Oldest: ${test.peek("oldest")}`);
+console.log(`Newest: ${test.peek("newest")}`);
+console.log(`Highest priority: ${test.peek("highest")}`);
+console.log(`Lowest priority: ${test.peek("lowest")}`);
+
+console.log(test.queue);
+
+console.log(`Oldest delete: ${test.dequeue("oldest")}`);
+console.log(`Newest delete: ${test.dequeue("newest")}`);
+console.log(`Highest delete: ${test.dequeue("highest")}`);
+console.log(`Lowest delete: ${test.dequeue("lowest")}`);
 
 console.log(test.queue);
