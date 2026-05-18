3fe8168ba3459cd824ec31cfd72039eefbdc888c
Vvvvvaleriia <reznikvaleria31@gmail.com>
2026-05-18 22:01:48 +0300

    Add example to show how emitter works

--- lab7/emitter.js
+++ lab7/emitter.js
@@ -21,3 +21,27 @@ class EventEmitter {
 		}
 	}
 }
+
+function greeting(name) {
+	console.log(`Hello ${name}`);
+}
+
+function authoriz(name, yearOfBirth) {
+	console.log(`Your password is ${name}${yearOfBirth}`);
+}
+
+function logged() {
+	console.log("Now you are logged");
+}
+
+const events = new EventEmitter();
+
+events.on("Authorization", greeting);
+events.on("Authorization", authoriz);
+events.on("Authorization", logged);
+
+events.emit("Authorization", "Valeriia", "2008");
+
+console.log("----------------------");
+events.off("Authorization", logged);
+events.emit("Authorization", "Valeriia", "2008");
